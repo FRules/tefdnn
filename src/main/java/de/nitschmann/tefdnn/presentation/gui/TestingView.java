f6e6661bb11b2366c6b654ec4d1f49423dcd5d8b
FRules <dominik.nitschmann@googlemail.com>
2018-07-26 08:27:29 +0200

    Drag&Drop is now also available on a jlabel with fixed dimensions

--- src/main/java/de/nitschmann/tefdnn/presentation/gui/TestingView.java
+++ src/main/java/de/nitschmann/tefdnn/presentation/gui/TestingView.java
@@ -25,106 +25,92 @@ public class TestingView extends JFrame implements ActionListener {
     private TrainingEnvironment trainedEnvironment;
     private ImageLoader imageLoader;
     private JLabel imagePreview;
-    GridBagConstraints c = new GridBagConstraints();
+    private GridBagConstraints c = new GridBagConstraints();
     private ArrayList<JLabel> neuronNumberList = new ArrayList<>();
     private ArrayList<JLabel> neuronValueList = new ArrayList<>();
+
+    private final int widthOfImage = 600;
+    private final int heightOfImage = 200;
 
     public TestingView(TrainingEnvironment trainedEnvironment) {
         this.trainedEnvironment = trainedEnvironment;
         this.imageLoader = new ImageLoader(trainedEnvironment.getFeedForwardNetwork());
 
-        JFrame self = this;
-
-        inputField = new JTextField();
-        inputField.setText("Enter path to image or drag and drop...");
-        testButton = new JButton("Test image...");
-        testButton.addActionListener(this);
+        this.inputField = new JTextField();
+        this.inputField.setText("Enter path to image or drag and drop...");
+        this.testButton = new JButton("Test image...");
+        this.testButton.addActionListener(this);
 
         this.imagePreview = new JLabel();
+        this.imagePreview.setText("Drag and drop image here");
+        this.imagePreview.setMinimumSize(new Dimension(this.widthOfImage, this.heightOfImage));
+        this.imagePreview.setPreferredSize(new Dimension(this.widthOfImage, this.heightOfImage));
+        this.imagePreview.setMaximumSize(new Dimension(this.widthOfImage, this.heightOfImage));
+        this.imagePreview.setHorizontalAlignment(SwingConstants.CENTER);
+        this.imagePreview.setVerticalAlignment(SwingConstants.CENTER);
 
-        panel = new JPanel(new GridBagLayout());
+        this.panel = new JPanel(new GridBagLayout());
 
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.gridx = 0;
-        c.gridy = 0;
-        panel.add(inputField, c);
+        this.c.fill = GridBagConstraints.HORIZONTAL;
+        this.c.gridx = 0;
+        this.c.gridy = 0;
+        this.panel.add(inputField, c);
 
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.gridx = 1;
-        c.gridy = 0;
-        panel.add(testButton, c);
+        this.c.fill = GridBagConstraints.HORIZONTAL;
+        this.c.gridx = 1;
+        this.c.gridy = 0;
+        this.panel.add(testButton, c);
 
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.gridwidth = 2;
-        c.gridx = 0;
-        c.gridy = 1;
-        panel.add(imagePreview, c);
+        this.c.fill = GridBagConstraints.HORIZONTAL;
+        this.c.gridwidth = 2;
+        this.c.gridx = 0;
+        this.c.gridy = 1;
+        this.panel.add(imagePreview, c);
 
         new DropTarget(inputField, new DropTargetListener() {
             @Override
-            public void drop(DropTargetDropEvent dtde)
-            {
-                try
-                {
-                    Transferable tr = dtde.getTransferable();
-                    DataFlavor[] flavors = tr.getTransferDataFlavors();
-
-                    for (int i = 0; i < flavors.length; i++)
-                    {
-                        if (flavors[i].isFlavorJavaFileListType())
-                        {
-                            dtde.acceptDrop(dtde.getDropAction());
-                            @SuppressWarnings("unchecked")
-                            java.util.List<File> files = (java.util.List<File>) tr.getTransferData(flavors[i]);
-                            if (files.size() > 1) {
-                                JOptionPane.showMessageDialog(panel, "Es kann nur eine einzelne Datei getestet werden.");
-                                dtde.dropComplete(true);
-                                return;
-                            }
-
-                            for (int k = 0; k < files.size(); k++)
-                            {
-                                inputField.setText(files.get(k).toString());
-                            }
-
-                            dtde.dropComplete(true);
-                        }
-                    }
-                    BufferedImage img = null;
-                    try {
-                        img = ImageIO.read(new File(inputField.getText()));
-                    } catch (IOException e) {
-                        System.out.println(e.getMessage());
-                    }
-
-                    imagePreview.setIcon(new ImageIcon(getScaledImage(img, 600, 200)));
-                    removeOldNeuronLabels();
-                    self.pack();
-                    return;
-                }
-                catch (Throwable t)
-                {
-                    t.printStackTrace();
-                }
-                dtde.rejectDrop();
-
+            public void drop(DropTargetDropEvent dtde) {
+                dropped(dtde);
             }
 
             @Override
-            public void dragEnter(DropTargetDragEvent dtde)
-            {}
+            public void dragEnter(DropTargetDragEvent dtde) {
+            }
 
             @Override
-            public void dragOver(DropTargetDragEvent dtde)
-            {}
+            public void dragOver(DropTargetDragEvent dtde) {
+            }
 
             @Override
-            public void dropActionChanged(DropTargetDragEvent dtde)
-            {}
+            public void dropActionChanged(DropTargetDragEvent dtde) {
+            }
 
             @Override
-            public void dragExit(DropTargetEvent dte)
-            {}
+            public void dragExit(DropTargetEvent dte) {
+            }
+        });
+
+        new DropTarget(imagePreview, new DropTargetListener() {
+            @Override
+            public void drop(DropTargetDropEvent dtde) {
+                dropped(dtde);
+            }
+
+            @Override
+            public void dragEnter(DropTargetDragEvent dtde) {
+            }
+
+            @Override
+            public void dragOver(DropTargetDragEvent dtde) {
+            }
+
+            @Override
+            public void dropActionChanged(DropTargetDragEvent dtde) {
+            }
+
+            @Override
+            public void dragExit(DropTargetEvent dte) {
+            }
         });
 
         this.setTitle("Test image");
@@ -133,7 +119,68 @@ public class TestingView extends JFrame implements ActionListener {
         this.setVisible(true);
     }
 
-    private Image getScaledImage(Image srcImg, int w, int h){
+    private void dropped(DropTargetDropEvent dtde) {
+        try {
+            Transferable tr = dtde.getTransferable();
+            DataFlavor[] flavors = tr.getTransferDataFlavors();
+
+            for (int i = 0; i < flavors.length; i++) {
+                if (flavors[i].isFlavorJavaFileListType()) {
+                    dtde.acceptDrop(dtde.getDropAction());
+                    @SuppressWarnings("unchecked")
+                    java.util.List<File> files = (java.util.List<File>) tr.getTransferData(flavors[i]);
+                    if (files.size() > 1) {
+                        JOptionPane.showMessageDialog(panel, "Es kann nur eine einzelne Datei getestet werden.");
+                        dtde.dropComplete(true);
+                        return;
+                    }
+
+                    for (int k = 0; k < files.size(); k++) {
+                        if (!files.get(k).toString().endsWith(".jpg") &&
+                                !files.get(k).toString().endsWith(".jpeg") &&
+                                !files.get(k).toString().endsWith(".bmp") &&
+                                !files.get(k).toString().endsWith(".gif") &&
+                                !files.get(k).toString().endsWith(".png")) {
+                            JOptionPane.showMessageDialog(panel,
+                                    "Fehler beim Lesen der Bilddatei. Vermutlich wurde ein falsches Dateiformat angegeben.\n" +
+                                            "Unterstützte Formate: .jpg, .jpeg, .png, .bmp, .gif");
+                            dtde.dropComplete(true);
+                            return;
+                        }
+                        inputField.setText(files.get(k).toString());
+                    }
+
+                    dtde.dropComplete(true);
+                }
+            }
+            BufferedImage img;
+            try {
+                img = ImageIO.read(new File(inputField.getText()));
+            } catch (IOException e) {
+                JOptionPane.showMessageDialog(panel, e.getStackTrace());
+                return;
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(panel, e.getStackTrace());
+                return;
+            }
+
+            if (img == null) {
+                JOptionPane.showMessageDialog(panel, "Bilddatei ist vermutlich beschädigt und konnte nicht geladen werden");
+                return;
+            }
+
+            imagePreview.setIcon(new ImageIcon(getScaledImage(img, this.widthOfImage, this.heightOfImage)));
+            imagePreview.setText("");
+
+            this.test();
+            return;
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
+        dtde.rejectDrop();
+    }
+
+    private Image getScaledImage(Image srcImg, int w, int h) {
         BufferedImage resizedImg = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
         Graphics2D g2 = resizedImg.createGraphics();
 
@@ -147,39 +194,43 @@ public class TestingView extends JFrame implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource().equals(testButton)) {
-            removeOldNeuronLabels();
-
-            String path = inputField.getText();
-
-            if (!imageLoader.setTestImage(path)) {
-                return;
-            }
-            double[] testData = imageLoader.getTestImage(trainedEnvironment.getFeedForwardNetwork().getMeanImage());
-
-            trainedEnvironment.getFeedForwardNetwork().setInput(trainedEnvironment.getFeedForwardNetwork(), testData);
-            ArrayList<Double> result = trainedEnvironment.getFeedForwardNetwork().test(trainedEnvironment.getFeedForwardNetwork());
-            ArrayList<Neuron> outputNeurons = trainedEnvironment.getFeedForwardNetwork().getOutputLayer().getNeurons();
-            for(int i = 0; i < result.size(); i++) {
-                JLabel neuronNumber = new JLabel(outputNeurons.get(i).getName());
-                JLabel neuronValue = new JLabel(String.format("%.4f",result.get(i) * 100) + " %");
-                c.fill = GridBagConstraints.HORIZONTAL;
-                c.gridx = 0;
-                c.gridy = 2 + i;
-                panel.add(neuronNumber, c);
-
-                c.fill = GridBagConstraints.HORIZONTAL;
-                c.gridx = 1;
-                c.gridy = 2 + i;
-                panel.add(neuronValue, c);
-                neuronNumberList.add(neuronNumber);
-                neuronValueList.add(neuronValue);
-            }
-            this.pack();
+            test();
         }
     }
 
+    private void test() {
+        removeOldNeuronLabels();
+
+        String path = inputField.getText();
+
+        if (!imageLoader.setTestImage(path)) {
+            return;
+        }
+        double[] testData = imageLoader.getTestImage(trainedEnvironment.getFeedForwardNetwork().getMeanImage());
+
+        trainedEnvironment.getFeedForwardNetwork().setInput(trainedEnvironment.getFeedForwardNetwork(), testData);
+        ArrayList<Double> result = trainedEnvironment.getFeedForwardNetwork().test(trainedEnvironment.getFeedForwardNetwork());
+        ArrayList<Neuron> outputNeurons = trainedEnvironment.getFeedForwardNetwork().getOutputLayer().getNeurons();
+        for (int i = 0; i < result.size(); i++) {
+            JLabel neuronNumber = new JLabel(outputNeurons.get(i).getName());
+            JLabel neuronValue = new JLabel(String.format("%.4f", result.get(i) * 100) + " %");
+            c.fill = GridBagConstraints.HORIZONTAL;
+            c.gridx = 0;
+            c.gridy = 2 + i;
+            panel.add(neuronNumber, c);
+
+            c.fill = GridBagConstraints.HORIZONTAL;
+            c.gridx = 1;
+            c.gridy = 2 + i;
+            panel.add(neuronValue, c);
+            neuronNumberList.add(neuronNumber);
+            neuronValueList.add(neuronValue);
+        }
+        this.pack();
+    }
+
     private void removeOldNeuronLabels() {
-        for(JLabel label : neuronNumberList) {
+        for (JLabel label : neuronNumberList) {
             panel.remove(label);
         }
         for (JLabel label : neuronValueList) {
@@ -187,7 +238,8 @@ public class TestingView extends JFrame implements ActionListener {
         }
     }
 
-    public @Override void toFront() {
+    public @Override
+    void toFront() {
         int sta = super.getExtendedState() & ~JFrame.ICONIFIED & JFrame.NORMAL;
 
         super.setExtendedState(sta);
